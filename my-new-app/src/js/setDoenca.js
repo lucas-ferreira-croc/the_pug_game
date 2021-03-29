8b057e2c54a2a0d8470114ff0a3c56200f77668b
Lucas Ferreira <lucas.ferreira057@bandtec.com.br>
2021-03-28 23:00:41 -0300

    game base

---
+++ my-new-app/src/js/setDoenca.js
@@ -0,0 +1,16 @@
+
+function braquicefalia(){
+  window.location.href = "html/predispoBraqui.html";
+}
+
+function dermatite(){
+  window.location.href = "html/predispoDerma.html";
+}
+
+function chosenBraqui(){
+    window.location.href = "Braqui.html";
+}
+
+function chosenDerma(){
+    window.location.href = "Derma.html"
+}
